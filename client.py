2fe4b76692a22ca614bccc9bf1f046f9711dbe32
Marshall Haymond <68209576+HaymondM@users.noreply.github.com>
2021-02-18 09:01:18 -0700

    Updated loop to exit game

--- client.py
+++ client.py
@@ -1,12 +1,15 @@
 #cleint
 import socket
-from tkinter import *
+#import main
+#from tkinter import *
 
 HOST = 'localhost'  # The server's hostname or IP address
 PORT = 65432        # The port used by the server
 play = True
 
+"""
 x = 'shoot'
+
 
 def setText():
     btn['text'] = 'Submitted'
@@ -24,23 +27,23 @@ btn = Button(root, text = 'Rock', bd = '5',
 btn.pack(side = 'top')    
  
 root.mainloop()
+"""
 
-print(x)
-
-while play == True:
+while play:
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    
         s.connect((HOST, PORT))
         print("Connected to the server: " + HOST)
-        user_input = input('Rock, papaer, or Scissor?').lower()
+        user_input = input('Rock,Paper, or Scissor?').lower()
         user_input = bytes(user_input, 'utf-8') 
         s.sendall(user_input)
         data = s.recv(1024)
         print(data.decode()) # print out result
         play_again = input("Play again?(Y/N)").lower()
         if play_again == 'n':
-            play = False
-            #break
+            stop = b'stop'
+            #s.close()
+            #s.connect((HOST, PORT))
+            s.sendall(stop)
+            #play = False
 
-
-   
+            break
